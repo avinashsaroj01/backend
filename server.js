87c97b26b4b11094eacc5f15b85816a0ea491deb
Avinash Saroj <98206280+avinashsaroj01@users.noreply.github.com>
2026-01-03 02:09:34 +0530

    feat: add CORS support to the application

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const connectDB = require("./config/db");
 const pasteRoutes = require("./route/pasteRoutes");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
